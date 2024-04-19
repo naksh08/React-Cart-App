078f0dd20f7bc62b74d99009387714ae9ae850a3
naksh08 <nakshatradeep21@gmail.com>
2024-04-19 12:09:22 +0530

    Adding state in CartItem

--- src/CardItem.js
+++ src/CardItem.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import add from "./images/add.png";
+import del from "./images/delete.png";
+import minus from "./images/minus.png";
 
 const CardItem = () => {
+  const  [data, setData]  = useState({
+    price: 999,
+    title: "Mobile Phone",
+    qty: 1,
+    img: "",
+  });
+
+  const { price, title, qty } = data;
+
   const styles = {
     image: {
       height: 110,
@@ -16,10 +28,27 @@ const CardItem = () => {
         <img style={styles.image} />
       </div>
       <div className="right-block">
-        <div style={{ fontSize: 25 }}>Phone</div>
-        <div style={{ color: "#777" }}>Rs 999</div>
-        <div style={{ color: "#777" }}>Qty: 1</div>
-        <div className="cart-item-actions">{/* Buttons */}</div>
+        <div style={{ fontSize: 25 }}>{title}</div>
+        <div style={{ color: "#777" }}>{price}</div>
+        <div style={{ color: "#777" }}>{qty}</div>
+        <div className="cart-item-actions">
+          {/* Buttons */}
+          <img
+            alt="increase"
+            className="action-icons"
+            src={add}
+          />
+          <img
+            alt="decrease"
+            className="action-icons"
+            src={minus}
+          />
+          <img
+            alt="delete"
+            className="action-icons"
+            src={del}
+          />
+        </div>
       </div>
     </div>
   );
